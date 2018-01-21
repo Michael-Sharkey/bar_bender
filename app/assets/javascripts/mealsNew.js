f43fd33333dbc67e2568a433c162f6f3312340d0
Michael-Sharkey <31968775+Michael-Sharkey@users.noreply.github.com>
2018-01-21 16:56:30 -0500

    fix new meal form styling

--- app/assets/javascripts/mealsNew.js
+++ app/assets/javascripts/mealsNew.js
@@ -2,14 +2,28 @@ $(document).ready(function(){
 
   const newMeal = document.getElementById('meal-photo');
   const fileButton = document.getElementById('file-button');
-  const fileExt = document.getElementById('file-name');
+  const goodMeal = document.getElementsByClassName('fa-thumbs-up')[0];
+  const healthBoolean = document.getElementById('healthy-boolean');
+  const eatButton = document.getElementById('eat-button');
+  const mealSubmit = document.getElementById('meal-submit');
 
   function mealPic() {
     fileButton.click();
   };
 
-  newMeal.addEventListener('click', function(){
-    mealPic();
-  });
+  function toggleHealth() {
+    healthBoolean.click();
+    goodMeal.classList.toggle('activated');
+  };
 
+  function submitMeal() {
+    mealSubmit.click();
+  }
+
+  newMeal.addEventListener('click', mealPic);
+  fileButton.addEventListener('change', function(){
+    newMeal.style.color = 'green';
+  });
+  goodMeal.addEventListener('click', toggleHealth);
+  eatButton.addEventListener('click', submitMeal);
 })
